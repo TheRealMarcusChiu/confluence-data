95c6d9c9b80a8f0f8f2d7fcccf28cf820de78414
Marcus Chiu <marcuschiu9@gmail.com>
2020-05-11 19:59:35 -0500

    initial commit

---
+++ data/main.py
@@ -0,0 +1,27 @@
+from bs4 import BeautifulSoup
+import json
+
+
+def parse(s):
+    n = {
+        'name': s.li.label.a.string,
+        'href': s.li.label.a['href'],
+        'children': []}
+
+    lu_tags = s.li.findAll('ul', recursive=False)
+    for lu_tag in lu_tags:
+        n['children'].append(parse(lu_tag))
+
+    if len(n['children']) == 0:
+        n['value'] = 10
+
+    return n
+
+
+with open('confluence-data.xml', 'r') as file:
+    data = file.read().replace('\n', '')
+    soup = BeautifulSoup(data, 'html.parser')
+    node = parse(soup)
+
+    with open('confluence-data.json', 'w') as fp:
+        json.dump(node, fp)
